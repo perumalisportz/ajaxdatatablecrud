679925ad7f767aa22de47166f263e8ade8baebb6
perumalisportz <115545734+perumalisportz@users.noreply.github.com>
2022-10-11 17:18:24 +0530

    first commit

---
+++ app/Http/Controllers/DataTableAjaxCRUDController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+ 
+use App\Models\Company;
+ 
+use Datatables;
+ 
+class DataTableAjaxCRUDController extends Controller
+{
+    
+    public function index()
+    {
+        if(request()->ajax()) 
+        {
+            return datatables()->of(Company::select('*'))
+            ->addColumn('action', 'company-action')
+            ->rawColumns(['action'])
+            ->addIndexColumn()
+            ->make(true);
+        }
+        return view('companies');
+    }
+      
+    
+    public function store(Request $request)
+    {  
+ 
+        $companyId = $request->id;
+ 
+        $company   =   Company::updateOrCreate(
+                    [
+                     'id' => $companyId
+                    ],
+                    [
+                    'name' => $request->name, 
+                    'email' => $request->email,
+                    'address' => $request->address
+                    ]);    
+                         
+        return Response()->json($company);
+ 
+    }
+      
+      
+    
+    public function edit(Request $request)
+    {   
+        $where = array('id' => $request->id);
+        $company  = Company::where($where)->first();
+      
+        return Response()->json($company);
+    }
+      
+     
+    public function destroy(Request $request)
+    {
+        $company = Company::where('id',$request->id)->delete();
+      
+        return Response()->json($company);
+    }
+}
